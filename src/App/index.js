044459cbcb29d6b90b72b0a9bda513ee4defa43e
Matius Hariman Natasampurna <matius@bambu.life>
2020-04-26 23:46:04 +0800

    :truck: feat: move hot reload to index.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
@@ -29,4 +28,4 @@ function App() {
   );
 }
 
-export default hot(module)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import 'sanitize.css';
 
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { PersistGate } from 'redux-persist/integration/react';
-
-import 'sanitize.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -15,18 +14,30 @@ import configureStore, { history } from './configureStore';
 
 const { store, persistor } = configureStore({});
 
-render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ConnectedRouter history={history}>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ConnectedRouter>
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root'),
-);
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConnectedRouter history={history}>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ConnectedRouter>
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+// HMR whenever <App /> changes
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render();
+  });
+}
+
+// render app
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
